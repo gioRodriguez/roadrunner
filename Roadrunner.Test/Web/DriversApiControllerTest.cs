ed758334b3990d60d99a20567d5323e4a4cf8823
Giovannny <gio.rodriguez88@gmail.com>
2019-02-24 22:04:44 -0600

    driver hub

--- Roadrunner.Test/Web/DriversApiControllerTest.cs
+++ Roadrunner.Test/Web/DriversApiControllerTest.cs
@@ -6,7 +6,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Roadrunner.BusinessInterfaces;
 using Roadrunner.Types;
-using Roadrunner.Web.Controllers;
 using Roadrunner.Web.Hubs;
 using Roadrunner.Web.Models;
 
@@ -15,13 +14,11 @@ namespace Roadrunner.Test.Web
     [TestClass]
     public class DriversApiControllerTest
     {
-        private Mock<IHubContext<DriverHub>> _mockHub;
         private Mock<IDriversProcessor> _mockDriversProcessor;
 
         [TestInitialize]
         public void Setup()
         {
-            _mockHub = new Mock<IHubContext<DriverHub>>();
             _mockDriversProcessor = new Mock<IDriversProcessor>();
         }
 
@@ -29,7 +26,7 @@ namespace Roadrunner.Test.Web
         public void DriverReadyAtPositionTest()
         {
             // Arrange            
-            var driversApiCtrl = new DriversApiController(_mockHub.Object, _mockDriversProcessor.Object);
+            var driversApiCtrl = new DriverHub(_mockDriversProcessor.Object);
 
             // Act
             driversApiCtrl.DriverReadyAtPosition(new ReqPositionModel { X = 1, Y = 2 }).Wait();
